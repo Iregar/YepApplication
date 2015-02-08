27f73da63e87e3a380a1e86ae42cae154d75adaa
benjagarrido <benjamin.garrido@gmail.com>
2015-02-08 18:20:24 +0100

    benjaGarrido - Modificaciones clase Ernesto sábado 07, reformateo del código y modificación de strings

--- app/src/main/java/es/yepwarriors/yepwarriors/FriendsFragment.java
+++ app/src/main/java/es/yepwarriors/yepwarriors/FriendsFragment.java
@@ -1,7 +1,7 @@
 package es.yepwarriors.yepwarriors;
 
-import android.support.v4.app.ListFragment;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,8 +12,8 @@ import android.widget.ProgressBar;
 import com.parse.FindCallback;
 import com.parse.ParseException;
 import com.parse.ParseQuery;
-import com.parse.ParseUser;
 import com.parse.ParseRelation;
+import com.parse.ParseUser;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,16 +22,11 @@ import java.util.List;
  * Created by ivan on 6/02/15.
  */
 public class FriendsFragment extends ListFragment {
-
     final static String TAG = FriendsFragment.class.getName();
-
     List<ParseUser> mUsers;
     ArrayList<String> usernames;
-
     ArrayAdapter<String> adapter;
-
     ProgressBar spinner;
-
     ParseUser mCurrentUser;
     ParseRelation<ParseUser> mFriendsRelation;
 
@@ -39,7 +34,6 @@ public class FriendsFragment extends ListFragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_friends, container, false);
-
         spinner = (ProgressBar)
                 rootView.findViewById(R.id.progressBar);
         spinner.setVisibility(View.GONE);
@@ -49,49 +43,32 @@ public class FriendsFragment extends ListFragment {
     @Override
     public void onResume() {
         super.onResume();
-
         setListView();
-
         mCurrentUser = ParseUser.getCurrentUser();
-
         mFriendsRelation = mCurrentUser.getRelation(ParseConstant.FRIENDS_RELATION);
-
         ParseQuery query = ParseUser.getQuery();
         query.orderByAscending(ParseConstant.USERNAME);
-
-
         spinner.setVisibility(View.VISIBLE);
-
-
         mFriendsRelation.getQuery().findInBackground(new FindCallback<ParseUser>() {
-
             @Override
             public void done(List<ParseUser> users, ParseException e) {
-                if(e == null){
+                if (e == null) {
 //sucess
                     spinner.setVisibility(View.INVISIBLE);
                     mUsers = users;
-                    for(ParseUser user:users){
+                    for (ParseUser user : users) {
                         adapter.add(user.getUsername());
                     }
-
-                }
-                else{
+                } else {
                     Log.e(TAG, "ParseException caught: ", e);
                 }
             }
         });
-
     }
 
     private void setListView() {
-        usernames= new ArrayList<String>();
-
-        adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_1,usernames);
+        usernames = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, usernames);
         setListAdapter(adapter);
-
-
     }
-
-
 }
--- app/src/main/java/es/yepwarriors/yepwarriors/InboxFragment.java
+++ app/src/main/java/es/yepwarriors/yepwarriors/InboxFragment.java
@@ -1,8 +1,7 @@
 package es.yepwarriors.yepwarriors;
 
-import android.support.v4.app.ListFragment;
-import android.content.Context;
 import android.os.Bundle;
+import android.support.v4.app.ListFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,21 +10,16 @@ import android.widget.ProgressBar;
 /**
  * Created by ivan on 6/02/15.
  */
-public class InboxFragment extends ListFragment{
-
+public class InboxFragment extends ListFragment {
     ProgressBar progressBar;
-    Context context;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-       //le cargo un layout al fragmento
-        View rootView = inflater.inflate(R.layout.fragment_main,container,false);
-
-        //le pongo un proges bar y lo oculto
-           progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
+//le cargo un layout al fragmento
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+//le pongo un proges bar y lo oculto
+        progressBar = (ProgressBar) rootView.findViewById(R.id.progressBar);
         progressBar.setVisibility(View.INVISIBLE);
-
         return rootView;
     }
 }
