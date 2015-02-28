f232d970b97bf714d5900a0a6d192ae8c5ba2194
Luis <luis.salgado.lsm@gmail.com>
2015-02-28 12:15:50 +0100

    Actualizado y Funcionando 28/02

---
+++ app/src/main/java/es/yepwarriors/yepwarriors/MessageAdapter.java
@@ -0,0 +1,66 @@
+package es.yepwarriors.yepwarriors;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.parse.ParseObject;
+
+import java.util.List;
+
+
+
+
+public class MessageAdapter extends ArrayAdapter<ParseObject>{
+
+    protected Context mContext;
+    protected List<ParseObject> mMessages;
+
+    public MessageAdapter(Context context, List<ParseObject> messages){
+        super(context,R.layout.message_item, messages);
+        mContext = context;
+        mMessages = messages;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        Viewholder holder;
+
+        if(convertView==null) {
+            convertView = LayoutInflater.from(mContext).inflate(R.layout.message_item, null);
+
+            holder = new Viewholder();
+            holder.iconImageView = (ImageView) convertView.findViewById(R.id.messageIcon);
+            holder.nameLabel = (TextView) convertView.findViewById(R.id.senderLabel);
+
+            convertView.setTag(holder);
+        }
+        else{
+            holder= (Viewholder)convertView.getTag();
+
+        }
+
+        ParseObject message = mMessages.get(position);
+
+        if(message.getString(Constantes.ParseClasses.Messages.KEY_FILE_TYPE).equals(Constantes.FileTypes.IMAGE)) {
+            holder.iconImageView.setImageResource(R.drawable.ic_action_picture);
+        }
+        else{
+            holder.iconImageView.setImageResource(R.drawable.ic_action_play_over_video);
+        }
+        holder.nameLabel.setText(message.getString(Constantes.ParseClasses.Messages.KEY_SENDER_NAME));
+        return convertView;
+
+
+    }
+
+    public static class Viewholder{
+        ImageView iconImageView;
+        TextView nameLabel;
+
+    }
+}
